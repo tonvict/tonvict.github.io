1ea763cdb4edb66c739f3f12300ebcb556c913dd
Eytan Raphaely <eytan.raphaely@gmail.com>
2018-04-27 13:54:28 -0700

    asd

--- index.js
+++ index.js
@@ -2,58 +2,105 @@
 //'use strict';
 
 (function () {
-  
-    function $(id) { return document.getElementById(id) }
-    function qs(selector) { return document.querySelector(selector) }
-    function qsa(selector) { return document.querySelectorAll(selector) }
-    
 
-  
-
-    window.onload = function() {
-        unfade(qs("html"));
-        $("microphone").onclick = showJokePage;
-        $("home").onclick= homePage;
-        $("home2").onclick= homePage;
-        $("laptop").onclick = showCompPage;
-      
-      
-      
-      }
-  
-  
-    function homePage(){
-            $("mainpage").classList.remove("hidden");
-            $("jokepage").classList.add("hidden");
-            $("comp-page").classList.add("hidden");
+    function idz(id) {
+        return document.getElementById(id)
     }
 
-    function showJokePage (){
-            
-            $("mainpage").classList.add("hidden");
-            $("jokepage").classList.remove("hidden");
+    function qs(selector) {
+        return document.querySelector(selector)
     }
 
-    function showCompPage(){
-        $("mainpage").classList.add("hidden");
-        $("comp-page").classList.remove("hidden");
+    function qsa(selector) {
+        return document.querySelectorAll(selector)
     }
 
 
-    function unfade(element) {
-        let op = 0.1;  // initial opacity
+
+
+    window.onload = function () {
+
+        unfade(qs("header"), 20);
+        idz("microphone").onclick = showJokePage;
+        idz("home").onclick = homePage;
+        idz("home2").onclick = homePage;
+        idz("laptop").onclick = showCompPage;
+
+
+
+
+
+    }
+
+
+
+
+
+
+    function homePage() {
+        idz("mainpage").classList.remove("hidden");
+        idz("jokepage").classList.add("hidden");
+        idz("comp-page").classList.add("hidden");
+
+    }
+
+    function showJokePage() {
+
+        idz("mainpage").classList.add("hidden");
+        idz("jokepage").classList.remove("hidden");
+    }
+
+    function showCompPage() {
+        idz("mainpage").classList.add("hidden");
+        idz("comp-page").classList.remove("hidden");
+
+    }
+
+
+    function unfade(element, initial) {
+        let op = 0.001; // initial opacity
         //element.style.display = 'block';
         let timer = setInterval(function () {
-            if (op >= 1){
+            if (op >= 1) {
                 clearInterval(timer);
             }
             element.style.opacity = op;
             element.style.filter = 'alpha(opacity=' + op * 100 + ")";
             op += op * 0.05;
-        }, 10);
+        }, initial);
     }
-  
     
+    $(document).ready(function(){
+  // Add scrollspy to <body>
+ 
+
+  // Add smooth scrolling on all links inside the navbar
+  $("#collapsibleNavbar a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, jokepage').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }  // End if
+  });
+});
+
+
+
+
+
 
 
 
